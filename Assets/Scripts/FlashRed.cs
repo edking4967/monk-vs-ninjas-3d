0019fedf131f3f8487dc1a39f241d169eb044131
Edward King <ed.king4967@gmail.com>
2016-07-04 17:54:37 -0600

    started adding markers for targeting

--- Assets/Scripts/FlashRed.cs
+++ Assets/Scripts/FlashRed.cs
@@ -2,15 +2,69 @@
 using System.Collections;
 
 public class FlashRed : MonoBehaviour {
+    
+    private int timerInterval = 0;
+    private int startTime = 0;
+    private Color startColor;
+    private GameObject marker;
 
 	// Use this for initialization
 	void Start () {
-        Color redColor = new Color(1.0f,0.0f,0.0f,1.0f);
-        GetComponentsInChildren<Renderer>()[0].material.SetColor("_Color", redColor);
+        // Save original color
+        startColor = GetComponentsInChildren<Renderer>()[0].material.GetColor("_Color");
+        marker = GameObject.Find("Marker");
+        marker.GetComponent<Renderer>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if( (int) (Time.time * 1000) - startTime >= timerInterval )
+        {
+            clearTint();
+        }
+        // TODO: only set once
 	}
+
+    void setColor(Color c)
+    {
+        GetComponentsInChildren<Renderer>()[0].material.SetColor("_Color", c);
+    }
+
+    Color getColor()
+    {
+        return GetComponentsInChildren<Renderer>()[0].material.GetColor("_Color")
+    }
+
+    void clearTint()
+    {
+        if( getColor() == startColor)
+            return;
+        else
+            setColor(startColor);
+    }
+
+    void setTimer(int msec, int startTime)
+    {
+       this.timerInterval = msec; 
+       this.startTime = startTime;
+    }
+
+    public void flashForMsec(int msec)
+    {
+        Color redColor = new Color(1.0f,0.0f,0.0f,1.0f);
+        setColor(redColor);
+        setTimer(msec, (int) (Time.time * 1000) );
+        setMarkerOn();
+    }
+
+    // Can only have one marker max!
+    public void setMarkerOn()
+    {
+        marker.GetComponent<Renderer>().enabled = true;
+    }
+
+    public void setMarkerOff()
+    {
+        marker.GetComponent<Renderer>().enabled = false;
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkAround(15);	
+	}
+
+    void assignMarker(GameObject enemy)
+    {
+
+    }
+
+    void checkAround(int distance)
+    {
+        GameObject[] enemies =  GameObject.FindGameObjectsWithTag("Enemy"); 
+        ArrayList closeEnemies = new ArrayList();
+        foreach (GameObject enemy in enemies)
+        {
+            if(Vector3.Distance( gameObject.transform.position, enemy.transform.position) 
+                    < distance )
+            {
+                closeEnemies.Add(enemy); 
+                if (enemy.GetComponent<FlashRed>())
+                {
+                    enemy.GetComponent<FlashRed>().flashForMsec(750);
+                }
+            }
+        }
+
+    }
+}
